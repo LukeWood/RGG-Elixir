39ac0471baf16f312e0beeaaaef8936e8b6bc2ab
Luke Wood <lukewoodcs@gmail.com>
2018-04-01 13:01:14 -0500

    init

---
+++ lib/rgg.ex
@@ -0,0 +1,18 @@
+defmodule Rgg do
+  @moduledoc """
+  Documentation for Rgg.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rgg.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rgg_test.exs
@@ -0,0 +1,8 @@
+defmodule RggTest do
+  use ExUnit.Case
+  doctest Rgg
+
+  test "greets the world" do
+    assert Rgg.hello() == :world
+  end
+end
